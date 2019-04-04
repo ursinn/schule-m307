e188e72e5ec1b54dc9199dd7e073f5236331822f
Ursin Filli <ursin.filli@filli-it.ch>
2019-04-04 08:32:41 +0200

    1

---
+++ 1/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style type="text/css">
+      .gray {
+        background-color: #ccc;
+      }
+      td {
+        border: solid 1px black;
+      }
+      table {
+        border-collapse: collapse;
+      }
+      .activ {
+        color: red;
+        border-color: red;
+      }
+    </style>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td class="gray activ">Zürich</td>
+        <td class="gray">Lugano</td>
+        <td class="gray">Basel</td>
+        <td class="gray">Bern</td>
+        <td class="gray">Bellinzona</td>
+      </tr>
+      <tr>
+        <td class="gray">8723</td>
+        <td>432</td>
+        <td>8167</td>
+        <td>1623</td>
+        <td>864</td>
+      </tr>
+      <tr>
+        <td class="gray">32</td>
+        <td>91</td>
+        <td>0</td>
+        <td>81</td>
+        <td>29</td>
+      </tr>
+      <tr>
+        <td class="gray">98</td>
+        <td>65</td>
+        <td>0</td>
+        <td>14</td>
+        <td></td>
+      </tr>
+    </table>
+  </body>
+</html>

56f3bc882f29aa16b91ace50f57adc498a0eb022
Ursin Filli <ursin.filli@filli-it.ch>
2019-04-04 08:33:17 +0200

    3

---
+++ 3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo '<h1>Gitarrenlegende Eric Clapton</h1>';
+      echo '<p>Geboren am 30.03.1945 in Surrey, England.</p>';
+      echo '<p>Spitzname: Slowhand I englischer Blues- und Rock-Gitarrist.</p>';
+      echo '<p>Gilt als einer der "besten und bedeutendsten" Gitarristen.</p>';
+      echo '<p>Eine Liste der faszinierendsten Lieder wird zu lang ...</p>';
+      echo '<p><b>Filmauftritte: u.a. "Blues Brothers 2000", Rockoper "Tommy".</b></p>';
+    ?>
+  </body>
+</html>

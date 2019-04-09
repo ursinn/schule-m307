d56ee46a00c3c3b1130f9722930ce8a1d0ed48f2
Ursin Filli <ursin.filli@filli-it.ch>
2019-04-09 14:21:41 +0200

    10

---
+++ 10/form.php
@@ -0,0 +1,6 @@
+<?php
+$vorname = $_GET['vorname'];
+$nachname = $_GET['nachname'];
+
+echo "Hallo Welt <br> Ich bin {$vorname} {$nachname}";
+?>
---
+++ 10/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="10/form.php" method="get">
+      <p>Vorname <input type="text" name="vorname"></p>
+      <p>Nachname <input type="text" name="nachname"></p>
+      <input type="submit">
+      <input type="reset">
+    </form>
+  </body>
+</html>
